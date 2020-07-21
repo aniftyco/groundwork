bacaee53329e60dbd1afefd912499d762c1d4142
Josh Manders <josh@joshmanders.com>
2020-07-21 18:41:43 -0500

    flesh out groundwork

---
+++ src/bin.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { resolve } from 'path';
+import minimist from 'minimist';
+import chalk from 'chalk';
+import ora from 'ora';
+import { Groundwork } from '.';
+
+const {
+  _: [src, dest = '.'],
+  force = false,
+} = minimist(process.argv.slice(2), {
+  boolean: ['f', 'force'],
+  alias: { f: 'force' },
+});
+
+(async () => {
+  const groundwork = new Groundwork(src, resolve(dest), force);
+
+  const spinner = ora(`Fetching ${chalk.cyan(src)}...`).start();
+
+  await groundwork.fetch();
+
+  spinner.succeed();
+
+  spinner.start(`Laying ground work...`);
+
+  await groundwork.extract();
+
+  spinner.succeed();
+})().catch((err) => {
+  process.stderr.write(err.stack);
+  process.exit(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-// TODO: Build Groundwork!
+import { homedir } from 'os';
+import { join, dirname } from 'path';
+import { parse } from 'url';
+import { existsSync, createWriteStream } from 'fs';
+import { get as download } from 'https';
+import mkdirp from 'mkdirp';
+import tar from 'tar';
+
+export class Groundwork {
+  private tarFile: string;
+  private tarUrl: string;
+
+  constructor(src: string, private dest: string, private force = false) {
+    const cacheDir = join(homedir(), '.groundwork');
+    const { repo, hash } = this.parse(src);
+
+    this.tarFile = join(cacheDir, repo, `${hash}.tar.gz`);
+    this.tarUrl = `https://codeload.github.com/${repo}/tar.gz/${hash}`;
+  }
+
+  private parse(src: string) {
+    const info = parse(src);
+
+    return {
+      repo: info.pathname,
+      hash: (info.hash || '#master').slice(1),
+    };
+  }
+
+  public async fetch() {
+    if (!existsSync(this.tarFile) || this.force) {
+      return new Promise((resolve, reject) => {
+        download(this.tarUrl, async (response) => {
+          if (response.statusCode >= 400) {
+            return reject(response);
+          }
+
+          await mkdirp(dirname(this.tarFile));
+
+          const stream = createWriteStream(this.tarFile).on('finish', resolve);
+
+          response.pipe(stream);
+        }).on('error', reject);
+      });
+    }
+  }
+
+  public async extract() {
+    await mkdirp(this.dest);
+
+    await tar.extract({ strip: 1, file: this.tarFile, cwd: this.dest });
+  }
+}
