b6217f32708c0000d51e22b898d5b78efb9ef76b
Josh Manders <josh@joshmanders.com>
2020-12-19 12:38:50 -0600

    cleanup pr

--- src/index.ts
+++ src/index.ts
@@ -8,72 +8,45 @@ import mkdirp from 'mkdirp';
 import tar from 'tar';
 
 export class Groundwork {
+  private client: Octokit = new Octokit();
   private tarFile: string;
   private tarUrl: string;
-  private src: string;
   private cacheDir: string;
-  private initializationPromise: any;
 
-  constructor(src: string, private dest: string, private force = false) {
+  constructor(private src: string, private dest: string, private force = false) {
     this.cacheDir = join(homedir(), '.groundwork');
-    this.src = src;
-  }
-
-  private async doInitialize() {
-    const { repo, hash }: {repo: string; hash: string} = await this.parse(this.src);
-
-    this.tarFile = join(this.cacheDir, repo, `${hash}.tar.gz`);
-    this.tarUrl = `https://codeload.github.com/${repo}/tar.gz/${hash}`;
-  }
-
-  private async initialize() {
-    if (!this.initializationPromise) {
-      this.initializationPromise = this.doInitialize();
-    }
-    return this.initializationPromise;
-  }
-
-  private async parse(src: string) {
-    const info = parse(src);
-    const hash = await this.getLatestRelease(info.pathname);
-
-    return {
-      repo: info.pathname,
-      hash: (info.hash || `#${hash}`).slice(1),
-    };
   }
 
   private async getLatestRelease(src: string): Promise<string> {
-    const octokit = new Octokit();
-
     try {
-      const release = await octokit.request(
-        `GET /repos/${src}/releases/latest`
-      );
-  
-      return release.data.tag_name;
-    } catch(err) {
+      const { data: release } = await this.client.request(`GET /repos/${src}/releases/latest`);
+
+      return release.tag_name;
+    } catch (err) {
       return this.getDefaultBranch(src);
     }
   }
 
   private async getDefaultBranch(src: string): Promise<string> {
-    const octokit = new Octokit();
-
     try {
-      const release = await octokit.request(
-        `GET /repos/${src}`
-      );
-  
-      return release.data.default_branch;
-    } catch(err) {
+      const { data: release } = await this.client.request(`GET /repos/${src}`);
+
+      return release.default_branch;
+    } catch (err) {
       throw err;
     }
   }
 
-  public async fetch() {
-    await this.initialize();
+  public async parse() {
+    const { pathname: repo, hash: branch } = parse(this.src);
+    const release = await this.getLatestRelease(repo);
+    const hash = (branch || `#${release}`).slice(1);
 
+    this.tarFile = join(this.cacheDir, repo, `${hash}.tar.gz`);
+    this.tarUrl = `https://codeload.github.com/${repo}/tar.gz/${hash}`;
+  }
+
+  public async fetch() {
     if (!existsSync(this.tarFile) || this.force) {
       return new Promise((resolve, reject) => {
         download(this.tarUrl, async (response) => {
