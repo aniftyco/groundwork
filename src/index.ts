c6ec8b2f71b7b79c4014ac00119cec06ca3fbf7c
Geoff Selby <geoff.w.selby@gmail.com>
2020-12-18 22:04:08 -0600

    Default to latest release or default branch
    Default to the latest release of the src repo if any exist. Otherwise, use the default branch for the repo instead of assuming master.

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,77 @@ import { join, dirname } from 'path';
 import { parse } from 'url';
 import { existsSync, createWriteStream } from 'fs';
 import { get as download } from 'https';
+import { Octokit } from '@octokit/core';
 import mkdirp from 'mkdirp';
 import tar from 'tar';
 
 export class Groundwork {
   private tarFile: string;
   private tarUrl: string;
+  private src: string;
+  private cacheDir: string;
+  private initializationPromise: any;
 
   constructor(src: string, private dest: string, private force = false) {
-    const cacheDir = join(homedir(), '.groundwork');
-    const { repo, hash } = this.parse(src);
+    this.cacheDir = join(homedir(), '.groundwork');
+    this.src = src;
+  }
 
-    this.tarFile = join(cacheDir, repo, `${hash}.tar.gz`);
+  private async doInitialize() {
+    const { repo, hash }: {repo: string; hash: string} = await this.parse(this.src);
+
+    this.tarFile = join(this.cacheDir, repo, `${hash}.tar.gz`);
     this.tarUrl = `https://codeload.github.com/${repo}/tar.gz/${hash}`;
   }
 
-  private parse(src: string) {
+  private async initialize() {
+    if (!this.initializationPromise) {
+      this.initializationPromise = this.doInitialize();
+    }
+    return this.initializationPromise;
+  }
+
+  private async parse(src: string) {
     const info = parse(src);
+    const hash = await this.getLatestRelease(info.pathname);
 
     return {
       repo: info.pathname,
-      hash: (info.hash || '#master').slice(1),
+      hash: (info.hash || `#${hash}`).slice(1),
     };
   }
 
+  private async getLatestRelease(src: string): Promise<string> {
+    const octokit = new Octokit();
+
+    try {
+      const release = await octokit.request(
+        `GET /repos/${src}/releases/latest`
+      );
+  
+      return release.data.tag_name;
+    } catch(err) {
+      return this.getDefaultBranch(src);
+    }
+  }
+
+  private async getDefaultBranch(src: string): Promise<string> {
+    const octokit = new Octokit();
+
+    try {
+      const release = await octokit.request(
+        `GET /repos/${src}`
+      );
+  
+      return release.data.default_branch;
+    } catch(err) {
+      throw err;
+    }
+  }
+
   public async fetch() {
+    await this.initialize();
+
     if (!existsSync(this.tarFile) || this.force) {
       return new Promise((resolve, reject) => {
         download(this.tarUrl, async (response) => {
